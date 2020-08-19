f6491c7cf6e9940b26daaa72bd929a6932fa06b9
Devanshu Singh <65216265+devanshu06@users.noreply.github.com>
2020-08-19 17:28:18 +0530

    Update humanprogram.py

--- humanprogram.py
+++ humanprogram.py
@@ -7,116 +7,105 @@ pyttsx3.speak("chrome , notepad , mediaplayer , calculator , my pc , vlc media p
 print()
 while True:
     p = input("enter what you want to open :")
-    if(("chrome" in p) or ("google" in p) or ("browser" in p)) and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    if("chrome" in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("chrome will open in a second")
            os.system("start chrome")
            continue
-    elif(("chrome" in p) or ("google" in p) or ("browser" in p))and("close"in p):
+    elif("chrome" in p)and("close"in p):
         pyttsx3.speak("closing chrome")
         os.system("taskkill/im chrome.exe")
-
-    elif(("text") in p or ("editor") in p or ("notepad") in p) and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("notepad" in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("notepad will open in a second")
            os.system("start notepad")
            continue
-    elif(("text") in p or ("editor") in p or ("notepad") in p) and ("close"in p):
+    elif("notepad" in p) and ("close"in p):
         pyttsx3.speak("closing notepad")
         os.system("taskkill/im notepad.exe")
-
-    elif(("music" in p) or ("media" in p) or ("songs" in p) or ("song" in p)) and (("play" in p) or ("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("window media" in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("window media player will open in a second")
            os.system("start wmplayer")
            continue
-    elif (("music" in p) or ("media" in p) or ("songs" in p) or ("song" in p)) and ("close"in p):
+    elif ("window media" in p) and ("close"in p):
         pyttsx3.speak("closing window media player")
         os.system("taskkill/im wmplayer.exe")
-
-    elif(("calc" in p) or ("calculator" in p)) and (("open" in p) or ("run" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("calculator" in p) and (("open" in p) or ("run" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("calculator will open in a second")
            os.system("start calc")
            continue
-    elif (("calc" in p) or ("calculator" in p)) and ("close"in p):
+    elif ("calculator" in p) and ("close"in p):
         pyttsx3.speak("closing calculator")
         os.system("taskkill/im calculator.exe")
-
-    elif(("mycomputer" in p) or ("pc" in p)) and (("open" in p) or ("run" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("mycomputer" in p) and (("open" in p) or ("run" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("my computer will open in a second")
            os.system("start explorer")
            continue
-    elif (("mycomputer" in p) or ("pc" in p)) and ("close"in p):
+    elif ("mycomputer" in p) and ("close"in p):
         pyttsx3.speak("access denied to close")
-
-
-    elif(("video" in p) or ("videos" in p) or ("vlc" in p)) and (("play" in p) or ("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("vlc" in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("vlc media player will open in a second")
            os.system("start vlc")
            continue
-    elif (("video" in p) or ("videos" in p) or ("vlc" in p)) and ("close"in p):
+    elif ("vlc" in p) and ("close"in p):
         pyttsx3.speak("closing vlc media player")
         os.system("taskkill/im vlc.exe")
-
-    elif(("virtual") in p or ("terminal") in p or("machine")in p ) and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("virtualmachine"in p ) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("virtual machine will open in a second")
            os.system("start virtualbox")
            continue
-    elif (("virtual") in p or ("terminal") in p or ("machine") in p) and ("close"in p):
+    elif ("virtualmachine" in p) and ("close"in p):
         pyttsx3.speak("closing virtual machine")
         os.system("taskkill/im virtualbox.exe")
-
-
-    elif(("paint") in p or ("mspaint") in p or ("3d" in p ) ) and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("mspaint" in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("paint will open in a second")
            os.system("start mspaint")
            continue
-    elif (("paint") in p or ("ms") in p or ("3d" in p)) and ("close"in p):
+    elif ("mspaint" in p) and ("close"in p):
         pyttsx3.speak("closing paint")
         os.system("taskkill/im mspaint.exe")
-
-    elif(("task"in p) or ("manager" in p)) and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("task manager"in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
            pyttsx3.speak("task manager will open in a second")
            os.system("start taskmgr")
            continue
-    elif (("task" in p) or ("manager" in p)) and ("close"in p):
+    elif ("task manager" in p) and ("close"in p):
         pyttsx3.speak(" access denied to close")
-
-    elif("calendar" in p) and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont"in p)or("dont"in p)or("do not" in p):
+    elif("calendar" in p) and (("run" in p) or ("open" in p)):
+        if ("dont"in p):
            pyttsx3.speak("okk,as you say")
            pass
         else:
@@ -125,57 +114,52 @@ while True:
            continue
     elif ("calendar" in p) and ("close"in p):
         pyttsx3.speak(" access denied to close")
-
-    elif (("clock" in p) or ("time"in p))and (("show" in p) or ("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("clock" in p) and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("clock will open in a second")
             os.system("start ms-clock:")
             continue
-    elif (("clock" in p) or ("time" in p)) and ("close"in p):
+    elif ("clock" in p) and ("close"in p):
         pyttsx3.speak(" closing clock")
         os.system("taskkill/im time.exe")
-
-    elif (("camera" in p) or ("webcam"in p))and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("camera" in p) and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("camera will open in a second")
             os.system("start microsoft.windows.camera:")
             continue
-    elif (("camera" in p) or ("webcam" in p)) and ("close"in p):
+    elif ("camera" in p) and ("close"in p):
         pyttsx3.speak(" closing camera")
         os.system("taskkill/im windowscamera.exe")
-
-    elif (("mail" in p) or ("email"in p))and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("email"in p)and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("e-mail will open in a second")
             os.system("start outlookmail:")
             continue
-    elif (("mail" in p) or ("email" in p)) and ("close"in p):
+    elif ("email" in p) and ("close"in p):
         pyttsx3.speak(" closing mail")
         os.system("taskkill/im commsapps.exe")
-
-    elif (("maps" in p) or ("map"in p)or("location"in p))and (("show"in p)or("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif  (("map"in p)or("location"in p))and (("show"in p)or("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("map will open in a second")
             os.system("start bingmaps:")
             continue
-    elif (("maps" in p) or ("map" in p) or ("location" in p)) and ("close"in p):
+    elif (("map" in p) or ("location" in p)) and ("close"in p):
         pyttsx3.speak(" closing maps")
         os.system("taskkill/im maps.exe")
-
-    elif (("news" in p))and (("show"in p)or("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif (("news" in p))and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
@@ -185,74 +169,69 @@ while True:
     elif (("news" in p)) and ("close"in p):
         pyttsx3.speak(" closing news")
         os.system("taskkill/im Microsoft.Msn.News.exe")
-
-    elif (("screenshot" in p) or ("snap" in p) or("snip"in p)or("sketch"in p)) and (("take"in p) or("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("screenshot" in p) and (("take"in p) or("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("Snip & Sketch will open in a second")
             os.system("start ms-ScreenSketch:")
             continue
-    elif (("screenshot" in p) or ("snap" in p) or ("snip" in p)or("sketch"in p)) and ("close"in p):
+    elif ("screenshot" in p) and ("close"in p):
         pyttsx3.speak(" closing snip&sketch")
         os.system("taskkill/im ScreenSketch.exe")
-
-    elif (("excel" in p)or ("sheet"in p))and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("excel" in p)and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("microsoft excel will open in a second")
             os.system("start excel")
             continue
-    elif (("excel" in p) or ("sheet" in p)) and ("close"in p):
+    elif ("excel" in p) and ("close"in p):
         pyttsx3.speak(" closing msexcel")
         os.system("taskkill/im excel.exe")
-
-    elif (("word" in p)or ("office"in p))and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("msword" in p)and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("microsoft word will open in a second")
             os.system("start winword")
             continue
-    elif (("word" in p) or ("office" in p)) and ("close"in p):
+    elif ("msword" in p) and ("close"in p):
         pyttsx3.speak(" closing msword")
         os.system("taskkill/im winword.exe")
-
-    elif (("adobe" in p)or ("reader"in p)or ("pdf"in p)or("viewer"in p))and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("adobe reader"in p) and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("adobe reader will open in a second")
             os.system("start acrord32")
             continue
-    elif (("adobe" in p) or ("reader" in p) or ("pdf" in p) or ("viewer" in p)) and ("close"in p):
+    elif ("adobe reader" in p) and ("close"in p):
         pyttsx3.speak(" closing adobe reader")
         os.system("taskkill/im acrord32.exe")
-
-    elif (("powerpoint" in p)or ("presentation"in p))and (("run" in p) or ("open" in p) or ("excute" in p)):
-        if ("dont" in p) or ("dont" in p) or ("do not" in p):
+    elif ("powerpoint" in p)and (("run" in p) or ("open" in p)):
+        if ("dont" in p):
             pyttsx3.speak("okk,as you say")
             pass
         else:
             pyttsx3.speak("powerpoint will open in a second")
             os.system("start powerpnt")
             continue
-    elif (("powerpoint" in p) or ("presentation" in p)) and ("close"in p):
+    elif ("powerpoint" in p) and ("close"in p):
         pyttsx3.speak(" closing powerpoint")
         os.system("taskkill/im  powerpnt.exe")
-
-    elif (("exit" in p) or ("terminate" in p) or ("quit"in p)):
+    elif("exit" in p) or ("quit"in p):
         pyttsx3.speak("exiting the program. have a good day")
         print("Have a good day...")
         break
     else:
         pyttsx3.speak("unable to understand what you have written.")
         print("Not able to understand")
+
 
 
 
